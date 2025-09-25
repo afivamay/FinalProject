95a79a9a4234d41eb0af9896b0d828bd73b9d5b7
afivamay <afivamay14@gmail.com>
2025-09-25 08:40:40 +0700

    latihan 1 java

---
+++ latihan1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class latihan1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int lamaSewa, jarakTempuh;
+        int biayaMobil, biayaSupir, biayaBBM, totalBiaya;
+
+        System.out.println("Masukan berapa lama sewa: ");
+        lamaSewa = sc.nextInt();
+        
+        System.out.println("Masukan jarak tempuh: ");
+        jarakTempuh = sc.nextInt();
+
+        biayaMobil = 300000 * lamaSewa;
+        biayaSupir = 200000 * lamaSewa;
+        biayaBBM = 1000 * jarakTempuh;
+
+        totalBiaya = biayaMobil + biayaSupir + biayaBBM;
+        System.out.println("Jadi total biaya " + totalBiaya);
+
+        sc.close();
+    }
+}
